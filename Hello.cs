5160de8d4ab1353398a16c1021da0116b412f61b
Tony Morris <tmorris@tmorris.net>
2016-03-01 12:26:32 +1000

    functions

--- Hello.cs
+++ Hello.cs
@@ -2,6 +2,25 @@ using System;
 
 class C {
   public static void Main(string[] args) {
-    Console.WriteLine("hi");
+    int v = DoBoth("abc");
+    Console.WriteLine(gv);
+  }
+
+  static int DoBoth(string str) {
+    int r = DoubleLength(str);
+    int s = Add10(r);
+    return s;
+  }
+
+  static int DoubleLength(string str) {
+    int len = str.Length;
+    int len2 = len * 2;
+    return len2;
+  }
+
+  static int Add10(int i) {
+    int x = i + 10;
+    return x;
+    // return i + 10;
   }
 }
