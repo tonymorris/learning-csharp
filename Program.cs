e2eb8ebf0742f3be16ce666f5ce80b29310fc9ec
Tony Morris <tmorris@tmorris.net>
2016-03-30 10:49:58 +1000

    roll back

--- Program.cs
+++ Program.cs
@@ -4,214 +4,239 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Scoring {
-
-    class Program {
-
-        static void Main(string[] args) {
-
-            int[,] results ={
-                              { 4, 7, 9, 3, 8, 6},
-                              { 4, 8, 6, 4, 8, 5}
-
-                             };
-            
-            PrintCompetitors(results);
-            var judge = HighestJudge(results);
-            Console.WriteLine(Winner(Max(Scores(results))));
-            Console.WriteLine(JudgeStr(judge));
-            
-            Console.ReadLine();
-
-        }//end Main
-
-        static string Winner(Tuple<int, int> t)
+namespace First_Assignment
+{
+    class Program
+    {
+        // todo
+        // X display the result nicely
+        // loop, ask user again, do want to run again?
+        // move code out of Main
+        static void Main(string[] args)
         {
-            var r = new StringBuilder();
-
-            r.Append("the winner is competitor ");
-            r.Append(t.Item2 + 1);
-            r.Append(" with a score of ");
-            r.Append(t.Item1);
-
-            return r.ToString();
+            Loop();
         }
 
-        static int[] Scores(int[,] x)
+        static void Loop()
         {
-            var b = BeNice(x);
-            int[] r = new int[b.Length];
+            Compute();
 
-            for(int i = 0; i < b.Length; i++)
+            bool keepgoing = true;
+
+            while(keepgoing)
             {
-                r[i] = Score(b[i]);
-            }
-            return r;
-        }
+                bool hasagain = false;
 
-        static string JudgeStr(Tuple<int, double, int> t)
-        {
-            var r = new StringBuilder();
-
-            r.Append("the highest scoring judge ");
-            r.Append(t.Item3 + 1);
-            r.Append(" with a total of ");
-            r.Append(t.Item1);
-            r.Append(" and with the average grade of ");
-            r.Append(t.Item2);
-            
-            return r.ToString();
-        }
-
-        static void PrintCompetitors(int[,] x)
-        {
-            int[][] a = BeNice(x);
-
-            for (int i = 0; i < a.Length; i++)
-            {
-                Console.WriteLine(CompetitorStr(i, a[i]));
-                Console.WriteLine();
-            }
-        }
-
-        static string CompetitorStr(int i, int[] x)
-        {
-            var r = new StringBuilder();
-
-            r.Append("Competitor ");
-            r.Append(i + 1);
-            r.Append(" your scores are ");
-            r.Append(ArrayStr(x));
-            r.Append("\n        ");
-            r.Append("and your score was ");
-            r.Append(Score(x));
-
-            return r.ToString();
-        }
-
-        static string ArrayStr(int[] x)
-        {
-            var r = new StringBuilder();
-
-            foreach(int el in x)
-            {
-                r.Append(el);
-                r.Append(' ');   
-            }
-
-            return r.ToString();
-        }
-        // total, average, index
-        static Tuple<int, double, int> HighestJudge(int[,] x)
-        {
-            int total = 0;
-            int index = -1;
-
-            var y = BeNice(Transpose(x));
-            
-            for(int i = 0; i < y.Length; i++)
-            {
-                var tmm = TotalMinMax(y[i]);
-                if(tmm.Item1 > total)
+                while (!hasagain)
                 {
-                    total = tmm.Item1;
-                    index = i;
+                    Console.Write("Again? [Y/N] ");
+                    string line = Console.ReadLine();
+                    if (line.ToLower() == "n")
+                    {
+                        keepgoing = false;
+                        hasagain = true;
+                    }
+                    else if (line.ToLower() == "y")
+                    {
+                        Compute();
+                        hasagain = true;
+                    }
+
+                    if(!hasagain)
+                    {
+                        Console.WriteLine("Y or N dummy");
+                    }
                 }
             }
-           
-            double avg = total / y[0].Length;
-
-            return new Tuple<int, double, int>(total, avg, index);
         }
 
+        static void Compute()
+        {
+            Selection s = Welcome();
+
+            double result = ComputeProduct(s);
+            Console.WriteLine("The " + Display(s) + " is: " + result + " " + Units(s));
+        }
+
+        enum Selection
+        {
+            Voltage, Current, Resistance
+        }
+
+        static string Units(Selection s)
+        {
+            switch (s)
+            {
+                case Selection.Voltage: return "volts";
+                case Selection.Current: return "amperes";
+                case Selection.Resistance: return "ohms";
+
+                default: throw new Exception("Invalid Selection value: " + s);
+            }
+        }
+
+        static double ComputeProduct(Selection s)
+        {
+            Tuple<Selection, Selection> others = OtherSelections(s);
+            double other1 = CalculateSelection(others.Item1);
+            double other2 = CalculateSelection(others.Item2);
+            if (s == Selection.Voltage)
+            {
+                return other1 * other2;
+            }
+            else if (others.Item1 == Selection.Voltage)
+            {
+                return other1 / other2;
+            } else
+            {
+                return other2 / other1;
+            }
+        }
+        
+        static double Minimum(Selection s)
+        {
+            switch(s)
+            {
+                case Selection.Voltage: return 0.0;
+                case Selection.Current: return 0.01;
+                case Selection.Resistance: return 10;
+ 
+                default: throw new Exception("Invalid Selection value: " + s);
+            }
+        }
+
+        static string Display(Selection s)
+        {
+            switch (s)
+            {
+                case Selection.Voltage: return "voltage";
+                case Selection.Current: return "current";
+                case Selection.Resistance: return "resistance";
+
+                default: throw new Exception("Invalid Selection value: " + s);
+            }
+        }
+
+        static Tuple<Selection, Selection> OtherSelections(Selection s)
+        {
+            switch (s)
+            {
+                case Selection.Voltage: return new Tuple<Selection, Selection>(Selection.Current, Selection.Resistance);
+                case Selection.Current: return new Tuple<Selection, Selection>(Selection.Voltage, Selection.Resistance);
+                case Selection.Resistance: return new Tuple<Selection, Selection>(Selection.Voltage, Selection.Current);
+
+                default: throw new Exception("Invalid Selection value: " + s);
+            }
+        }
+
+        static Selection Welcome()
+        {
+            int valuei = default(int);
+            bool hasValue = false;
+
+            Console.WriteLine("Welcome to Ohms Law Calculator");
+            Console.WriteLine("[1] Calculate Voltage");
+            Console.WriteLine("[2] Calculate Current");
+            Console.WriteLine("[3] Calculate Resistance");
+            Console.WriteLine("[4] Exit");
+            Selection s = default(Selection);
+
+            while (!hasValue)
+            {
+                Console.Write("Please Enter a Number: ");
+                string values = Console.ReadLine();
+                bool x = Int32.TryParse(values, out valuei);
+                
+                if(x)
+                {
+                    if(valuei == 1)
+                    {
+                        s = Selection.Voltage;
+                        hasValue = true;
+                    } else if(valuei == 2)
+                    {
+                        s = Selection.Current;
+                        hasValue = true;
+                    } else if(valuei == 3)
+                    {
+                        s = Selection.Resistance;
+                        hasValue = true;
+                    } else if(valuei == 4)
+                    {
+                        Environment.Exit(0);
+                    }
+                }
+              
+                if(!hasValue)
+                {
+                    Console.WriteLine("Please Enter a Number [1-4]");
+                }
+                
+            }
+
+            return s;
+        }
+
+        static double CalculateSelection(Selection s)
+        {
+            double r = default(double);
+            bool hasValue = false;
+
+            while (!hasValue)
+            {
+                Console.Write("Please enter " + Display(s) + ": ");
+                string line = Console.ReadLine();
+                bool x = Double.TryParse(line, out r);
+
+                if(x)
+                {
+                    if(r >= Minimum(s))
+                    {
+                        hasValue = true;
+                    } else
+                    {
+                        Console.WriteLine(r + " is below minimum " + Minimum(s) + " for " + Display(s));
+                    }
+                } else
+                {
+                    Console.WriteLine("Invalid value for " + Display(s));
+                }
+            }
+
+            return r;
+        }
 
         /*
-        Assume Length > 1.
+        static void CalculateVoltage()     
+        {
+            Console.Write("Please enter Current: ");
+            int currenti = 0;
+            while (currenti <= 0)
+            {
+                string current = Console.ReadLine();
+                bool x = Int32.TryParse(current, out currenti);
+                if (!x)
+                {
+                    Console.Write("Please enter a valid current: ");
+                }
+            }
+
+            int resistancei = 0;
+            while (resistancei <= 0)
+            {
+                Console.Write("Please enter Resistance: ");
+                string resistance = Console.ReadLine();
+                bool x = Int32.TryParse(resistance, out resistancei);
+                if (!x)
+                {
+                    Console.WriteLine("Please enter a valid Resistance: ");
+                }
+            }
+            Console.WriteLine("The Voltage is " + (currenti * resistancei));
+
+        }
         */
-        static int Score(int[] x)
-        {
-            Tuple<int, int, int> totalminmax = TotalMinMax(x);
+    }
 
-            return totalminmax.Item1 - totalminmax.Item2 - totalminmax.Item3;
-          
-        }
 
-        // index, maximum
-        static Tuple<int, int> Max(int[] t)
-        {
-            int max = -1;
-            int index = -1;
-
-            for(int i = 0; i < t.Length; i++)
-            {
-                if(t[i] > max)
-                {
-                    max = t[i];
-                    index = i;
-                }
-            }
-
-            return new Tuple<int, int>(max, index);
-        }
-
-        /*
-        Safe programming with arrays is not a thing.
-        Assume Length > 0.
-        Else have a bad day.
-        */
-        static Tuple<int, int, int> TotalMinMax(int[] t)
-        {
-            int min = t[0];
-            int max = t[0];
-            int total = t[0];
-
-            for(int i = 1; i < t.Length; i++)
-            {
-                if(t[i] < min)
-                {
-                    min = t[i];
-                }
-
-                if(t[i] > max)
-                {
-                    max = t[i];
-                }
-
-                total += t[i];
-            }
-
-            return new Tuple<int, int, int>(total, min, max);
-        }
-
-        static int[][] BeNice(int[,] x)
-        {
-            int[][] r = new int[x.GetLength(0)][];
-
-            for (int i = 0; i < x.GetLength(0); i++) {
-                r[i] = new int[x.GetLength(1)];
-                for(int j = 0; j < x.GetLength(1); j++)
-                {
-                    r[i][j] = x[i, j];
-                }
-            }
-
-            return r;
-        }
-
-        static int[,] Transpose(int[,] x)
-        {
-            int[,] r = new int[x.GetLength(1), x.GetLength(0)];
-
-            for(int i = 0; i < x.GetLength(1); i++)
-            {
-                for(int j = 0; j < x.GetLength(0); j++)
-                {
-                    r[i, j] = x[j, i];
-                }
-            }
-
-            return r;
-        }
-
-    }//end class
-}//end namespace
+}
